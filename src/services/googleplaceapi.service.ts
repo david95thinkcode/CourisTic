6f47c658813c70a3788a448d82ad4327532f6caf
David Hiheaglo <davidhihea@gmail.com>
2017-06-20 13:22:52 +0100

    TODO points were added

--- src/services/googleplaceapi.service.ts
+++ src/services/googleplaceapi.service.ts
@@ -8,10 +8,15 @@ import 'rxjs/add/operator/map';
 
 //my models
 import { GooglePlaceApiGlobal   }   from    '../models/googleplaceapi-global.model';
+import { GooglePlaceApiPlaceSearchResult   }   from    '../models/googleplaceapi-placesearchresult.model';
 
 @Injectable()
 export class GooglePlaceApiService {
-    private baseUrl: string = "https://maps.googleapis.com/maps/api/place/details/";
+    private baseURLDetail: string = "https://maps.googleapis.com/maps/api/place/details/";
+    private baseURLTextSearch = "https://maps.googleapis.com/maps/api/place/textsearch/";
+    private baseURLNearbySearch = "https://maps.googleapis.com/maps/api/place/nearbysearch/";
+    private baseURLRadarSearch = "https://maps.googleapis.com/maps/api/place/radarsearch/";
+
     private output: string = 'json?';
     private placeid: string = "ChIJ1zcFVeRUIxARZ86krdRQJwM";
     private apikey: string = "AIzaSyCLO_avsG1-B2kj5-FDfdE3CMguE2RESiY";
@@ -23,12 +28,9 @@ export class GooglePlaceApiService {
     public getDetails(): Promise<GooglePlaceApiGlobal> {
         const parameters = `placeid=${this.placeid}&key=${this.apikey}`;
 
-        //const url = `${this.baseUrl}json?placeid=${this.placeid}&key=${this.apikey}`;
-        
         //formation de l'url 
-        const url: string = this.baseUrl+this.output+parameters;
+        const url: string = this.baseURLDetail+this.output+parameters;
         
-        //console.log("parameters ==> : " + parameters);
         console.log("URL ==> : " + url);
 
         return this.http.get(url)
@@ -36,4 +38,55 @@ export class GooglePlaceApiService {
         .then(response => response.json() as GooglePlaceApiGlobal)
         .catch(error => console.log('Une erreur est survenue dans le service ' + error))
     }
+
+    /**
+     * Google place API - Text Search
+     * This method should receive a string of the place to search
+     * ############################## ########
+     */
+    public getTextSearchPlaces(): Promise<GooglePlaceApiPlaceSearchResult> {
+        
+        //query: string = "";
+        const parameters = `query=${this.query}&key=${this.apikey}`;
+        const url: string = this.baseURLTextSearch+this.output+parameters;
+        
+        return this.http.get(url)
+        .toPromise()
+        .then(response => response.json() as GooglePlaceApiPlaceSearchResult)
+        .catch(error => console.log('Une erreur est survenue dans le service ' + error))
+    }
+
+    /**
+     * Google place API - Nearby Search
+     * Parameters :
+     * - radius : Définit la distance (en mètres) jusqu'à laquelle renvoyer les résultats de lieu.
+     * - location : latitude,longitude.
+     * 
+     * ### #Facultatives parameters
+     * - type : string with which identifies the type of place we want to get
+     * - keyword : string 
+     * ############################## ########
+     */
+
+     //TODO: rechercher comment passer un paramètre string a cette méthode
+    public getNearbySearchPlaces(): Promise<GooglePlaceApiPlaceSearchResult> {
+        
+        const parameters = `query=${this.query}&key=${this.apikey}`;
+        const url: string = this.baseURLTextSearch+this.output+parameters;
+        
+        return this.http.get(url)
+        .toPromise()
+        .then(response => response.json() as GooglePlaceApiPlaceSearchResult)
+        .catch(error => console.log('Une erreur est survenue dans le service ' + error))
+    }
+
+    //TODO: Créer un methode getNearbySearchPlaces qui recoit les paramètres facultatifs
+    /**
+     * @Description
+     * La première recherchera les lieux proches de type restaurants dans un radius d'au plus 5000m (5km)
+     * La seconde recherchera les lieux proches de type hotels dans un radius d'au plus 5000m (5km)
+     * La troisième recherchera les lieux proches de type hotels dans un radius d'au plus 5000m (5km)
+     */
+
+    //TODO: 
 }
