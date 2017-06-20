884995fc414f372c20b3ec4bef512023a37aa612
David Hiheaglo <davidhihea@gmail.com>
2017-06-20 10:33:58 +0100

    Text Search method was added

---
+++ src/models/googleplaceapi-placesearchresult.model.ts
@@ -0,0 +1,13 @@
+/**
+ * Learn more on about this class structure on :
+ * https://developers.google.com/places/web-service/search?hl=fr#PlaceSearchResponses
+ */
+
+import { GooglePlaceApiResult   }   from './googleplaceapi-result.model';
+
+export class GooglePlaceApiPlaceSearchResult {
+    status: string;
+    html_attributions: string[];
+    results: GooglePlaceApiResult[];
+    next_page_token: string;
+}
