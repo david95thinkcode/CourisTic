4230ff4f3f5225dcdd05042a7d49f6775aa14967
David Hiheaglo <davidhihea@gmail.com>
2017-06-28 03:13:18 +0100

    Restaurants proches et hotels proches disponiblees désormais

--- src/pages/estimation/estimation.ts
+++ src/pages/estimation/estimation.ts
@@ -8,12 +8,15 @@ import { GooglePlaceApiService }               from '../../services/googleplacea
 import { GooglePlaceApiGlobal }                from '../../models/googleplaceapi-global.model';
 import { GoogleMapsApiGlobal }                 from '../../models/googlemapsapi-global.model';
 import { GooglePlaceApiResult   }              from '../../models/googleplaceapi-result.model';
+import { GooglePlaceApiPlaceSearchResult   }   from '../../models/googleplaceapi-placesearchresult.model';
 import { GoogleMapsApiElement   }              from '../../models/googlemapsapi-element.model';
 import { GoogleMapsApiRow   }                  from '../../models/googlemapsapi-row.model';
 import { IonicNativeGeolocation }              from '../../models/ionicnative-geolocation.model';
 import { GoogleMapsApiDistance   }             from '../../models/googlemapsapi-distance.model';
-import { GoogleMapsApiDuration   }              from '../../models/googlemapsapi-duration.model';
+import { GoogleMapsApiDuration   }             from '../../models/googlemapsapi-duration.model';
 import { Trajet }                              from '../../models/trajet.model';
+import { Hotel }                               from '../../models/hotel.model';
+import { Restaurant }                          from '../../models/restaurant.model';
 
 @Component({
   selector: 'page-estimation',
@@ -23,16 +26,21 @@ import { Trajet }                              from '../../models/trajet.model';
 
 export class EstimationPage {
 
+  nombreHotel: number;
+  nombreRestaurant: number;
   trajet: Trajet = new Trajet();
   rows: GoogleMapsApiRow[];
   reponse: GoogleMapsApiGlobal = new GoogleMapsApiGlobal();
-
+  array_Hotel: Array<Hotel>;
+  array_restaurant: Array<Restaurant>;
+  
  constructor(public navCtrl: NavController, public googlePlaceApiService: GooglePlaceApiService, public googleMapsApiService: GoogleMapsApiService, public navParams: NavParams)
  {
    this.trajet.userDestination = navParams.get('userChoice');
    this.trajet.userPosition = navParams.get('currentLocation');
    this.Initialise();
    this.setDistanceDuration();
+   this.countNearbyPlaces();
  }
 
 
@@ -48,7 +56,12 @@ export class EstimationPage {
     this.trajet.duree_trajet = new GoogleMapsApiDuration();
     this.trajet.duree_trajet.text = "Indéterminé";
     this.trajet.distance_trajet.text = "Indéterminé";   
+    this.nombreHotel = 0;
+    this.nombreRestaurant = 0;
     
+    //TODO/ initialiser les deux tableau ci-dessous
+    //this.array_Hotel = 
+    //this.array_restaurant = 
   }
 
   /** EFFECTUE DES OPERATIONS POUR ATTRIBUER LA DUREE ET LA DISTANCE
@@ -67,7 +80,7 @@ export class EstimationPage {
     }
     //Destination absente
     else 
-    {      
+    { 
       this.googleMapsApiService.getDistanceMatrix(this.trajet.userPosition, this.trajet.userDestination)
       .then(fetched =>
       {
@@ -77,17 +90,16 @@ export class EstimationPage {
         //Aucun itinéraire n'a été trouvé 
         if ( this.rows[0].elements[0].status == "ZERO_RESULTS") {
           this.trajet.itineraire = false ;
-          this.trajet.distance_trajet.text = "Nous ne nouveau pas vous fournir d'information sur cet itinéraire";
         }
         //Itinéraire trouvé
         else 
         {
-          this.trajet.itineraire = true;
-          
+          this.trajet.itineraire = true;          
           //Récupération et affectation des données "distance" et "durée" au trajet
           this.trajet.distance_trajet = this.rows[0].elements[0].distance;
           this.trajet.duree_trajet = this.rows[0].elements[0].duration;
         }
+
        /* //OBSERVATION 
         console.log(this.rows);
         console.log(this.rows[0]);
@@ -103,6 +115,72 @@ export class EstimationPage {
 
   }
 
+  
+  /** Compte les lieux a proximité de la destination et informe 
+   * les variable nombreHotels et nombreRestaurant*/
+  private countNearbyPlaces() {
+
+    var response: GooglePlaceApiPlaceSearchResult = new GooglePlaceApiPlaceSearchResult();
+    var results: GooglePlaceApiResult[];
+    var totalplaces : number = 0;
+
+    //récuperer tous les lieux proches    
+    this.googlePlaceApiService.getNearbySearchPlaces(this.trajet.userDestination, 50000)
+    .then(fetched => 
+    {
+      response = fetched;      
+      results = response.results;
+      
+      //ON COMPTE LE NOMBRE
+      results.forEach(result => {
+        totalplaces ++;
+      });
+
+      //Observations
+      console.log("Nombre total : " + totalplaces);
+      console.log(results);
+      
+    })
+    .catch(error => console.log('countNearbyPlaces() error : ' + error));
+
+    //Hotels 
+    this.googlePlaceApiService.getNearbyHostels(this.trajet.userDestination, 0)
+    .then(fetched => 
+    {
+      response = fetched;      
+      results = response.results;
+      results.forEach(result => {
+        //On insère un hotel dans le tableau d'hotels
+        var un_hotel = new Hotel();
+        un_hotel.googleDetails = result;
+        //this.array_Hotel.push(un_hotel);
+
+        this.nombreHotel ++;
+        console.log("Hotel : " + this.nombreHotel);
+      });
+      
+    })
+    .catch(error => console.log('countNearbyPlaces() error : ' + error));
+    
+    //Restaurants
+    this.googlePlaceApiService.getNearbyRestaurants(this.trajet.userDestination, 0)
+    .then(fetched => 
+    {
+      response = fetched;      
+      results = response.results;
+      results.forEach(result => {
+        //On insère un restaurant dans le tableau de restaurants
+        var un_resto = new Restaurant();
+        un_resto.googleDetails = result;
+        //this.array_restaurant.push(un_resto);
+
+        this.nombreRestaurant ++;
+      });
+    })
+    .catch(error => console.log('countNearbyPlaces() error : ' + error));
+
+  }
+
   /** RENVOIE LE COUT ESTIME DU TRAJET
    * le cout renvoyé est composé de :
    * - cout de transport
@@ -110,7 +188,19 @@ export class EstimationPage {
    * - cout de restauration
    */
   private getGlobalPrice() {
+    
+  }
+
+  //TODO: implémenter la méthode
+  public showRestaurant(){
 
   }
+
+  //TODO: implémenter la méthode
+  public showHotels() {
+
+  }
+
+
 
 }
